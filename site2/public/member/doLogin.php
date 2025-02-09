2b8d5c7d2437e5a0a98aeeb9138f03a93e47196b
reroro1 <153210591+reroro1@users.noreply.github.com>
2025-02-10 01:41:27 +0900

    Update doLogin.php

--- site2/public/member/doLogin.php
+++ site2/public/member/doLogin.php
@@ -1,21 +1,50 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/web_init.php';
 
+if ( isset($_REQUEST['nickname']) == false ) {
+    $_REQUEST['nickname'] = '';
+}
+
+if ( empty($_REQUEST['nickname']) ) {
+    jsAlert("닉네임을 입력해주세요.");
+    jsHistoryBack();    
+}
+
 $sql = "
-SELECT *
+SELECT COUNT(*)
 FROM member
 WHERE loginId = '{$_REQUEST['loginId']}'
-AND loginPw = '{$_REQUEST['loginPw']}'
 ";
 
-$memberInfo = getRow($sql);
-
-if ( empty($memberInfo) ) {
-    jsAlert("일치하는 회원이 없습니다.");
+if ( getRowValue($sql) > 0 ) {
+    jsAlert("이미 사용중인 로그인 아이디 입니다.");
     jsHistoryBack();
 }
 
-$_SESSION['loginedMemberInfo'] = $memberInfo;
+$sql = "
+SELECT COUNT(*)
+FROM member
+WHERE nickname = '{$_REQUEST['nickname']}'
+";
 
-jsAlert('로그인 되었습니다.');
-jsLocationReplace('/article/list.php');
+if ( getRowValue($sql) > 0 ) {
+    jsAlert("이미 사용중인 닉네임 입니다.");
+    jsHistoryBack();
+}
+
+$plainLoginPw = $_REQUEST['loginPw'];  // 사용자가 입력한 비밀번호
+$hashedLoginPw = password_hash($plainLoginPw, PASSWORD_DEFAULT);  
+// PHP 내장 함수로 해싱 (기본 알고리즘: bcrypt)
+
+// 이후 INSERT 시 $hashedLoginPw 를 저장
+$sql = "
+INSERT INTO member
+SET regDate = NOW(),
+loginId = '{$_REQUEST['loginId']}',
+loginPw = '{$hashedLoginPw}',
+nickname = '{$_REQUEST['nickname']}'
+";
+execute($sql);
+
+jsAlert('가입이 완료 되었습니다.');
+jsLocationReplace('/member/login.php');
