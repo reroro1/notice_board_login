c141bbc3e20530536c836cd53cbc0f12b4e522cf
reroro1 <153210591+reroro1@users.noreply.github.com>
2025-02-11 17:22:36 +0900

    Update doWrite.php

--- site2/public/article/doWrite.php
+++ site2/public/article/doWrite.php
@@ -1,34 +1,31 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/web_init.php';
 
-if ( isLogined() == false ) {
+if (!isLogined()) {
     jsAlert("로그인 후 이용해주세요.");
     jsHistoryBack();
 }
 
-if ( isset($_REQUEST['title']) == false ) {
-    $_REQUEST['title'] = '';
-}
+$title = $_REQUEST['title'] ?? '';
+$body = $_REQUEST['body'] ?? '';
 
-if ( empty($_REQUEST['title']) ) {
-    jsAlert("제목을 입력해주세요.");
-    jsHistoryBack();    
+if (empty($title) || empty($body)) {
+    jsAlert("모든 필드를 입력해주세요.");
+    jsHistoryBack();
 }
 
 $loginedMemberInfo = $_SESSION['loginedMemberInfo'];
 
-$sql = "
-INSERT INTO article
-SET regDate = NOW(),
-title = '{$_REQUEST['title']}',
-body = '{$_REQUEST['body']}',
-memberId = '{$loginedMemberInfo['id']}',
-writer = '{$loginedMemberInfo['nickname']}'
-";
+$conn = getDatabaseConnection();
 
-execute($sql);
-
-$newId = getLastInsertId();
+// 게시글 작성
+$sql = "INSERT INTO article (regDate, title, body, memberId, writer) VALUES (NOW(), ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssis", $title, $body, $loginedMemberInfo['id'], $loginedMemberInfo['nickname']);
+$stmt->execute();
+$newId = $stmt->insert_id;
+$stmt->close();
 
 jsAlert('글 작성이 되었습니다.');
 jsLocationReplace('/article/detail.php?id=' . $newId);
+?>
