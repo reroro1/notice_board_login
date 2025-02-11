c78692faf1a5345163fa95680693b16cfaa93cdf
reroro1 <153210591+reroro1@users.noreply.github.com>
2025-02-11 17:22:01 +0900

    Update doDelete.php

--- site2/public/article/doDelete.php
+++ site2/public/article/doDelete.php
@@ -1,40 +1,43 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/web_init.php';
 
-if ( isLogined() == false ) {
+if (!isLogined()) {
     jsAlert("로그인 후 이용해주세요.");
     jsHistoryBack();
 }
 
-if ( isset($_REQUEST['id']) == false ) {
-    $_REQUEST['id'] = '';
-}
+$id = $_REQUEST['id'] ?? '';
 
-if ( empty($_REQUEST['id']) ) {
+if (empty($id)) {
     jsAlert("삭제할 ID를 입력해주세요.");
-    jsHistoryBack();    
+    jsHistoryBack();
 }
 
 $loginedMemberInfo = $_SESSION['loginedMemberInfo'];
 
-$sql = "
-SELECT memberId
-FROM article
-WHERE id = '{$_REQUEST['id']}'
-";
-$articleMemberId = getRowValue($sql);
+$conn = getDatabaseConnection();
 
-if ( $articleMemberId != $loginedMemberInfo['id'] ) {
+// 게시글 작성자 확인
+$sql = "SELECT memberId FROM article WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$stmt->bind_result($articleMemberId);
+$stmt->fetch();
+$stmt->close();
+
+if ($articleMemberId != $loginedMemberInfo['id']) {
     jsAlert('삭제 권한이 없습니다.');
     jsHistoryBack();
 }
 
-$sql = "
-DELETE FROM article
-WHERE id = '{$_REQUEST['id']}'
-";
-
-execute($sql);
+// 게시글 삭제
+$sql = "DELETE FROM article WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$stmt->close();
 
 jsAlert('글이 삭제되었습니다.');
 jsLocationReplace('/article/list.php');
+?>
