bd3cc32b5bddb46c0603770c6abe202bc160d413
reroro1 <153210591+reroro1@users.noreply.github.com>
2025-02-11 17:54:55 +0900

    Update doLogout.php

--- site2/public/member/doLogout.php
+++ site2/public/member/doLogout.php
@@ -1,7 +1,10 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/web_init.php';
 
+$_SESSION = [];
 session_destroy();
+setcookie(session_name(), '', time() - 3600, '/');
 
 jsAlert('로그아웃 되었습니다.');
 jsLocationReplace('/member/login.php');
+?>
